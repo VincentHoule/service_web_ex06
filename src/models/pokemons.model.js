0b9f3f82cad0632890752c52c97e5140ad59352f
VincentHoule <2241013@carrefour.cegepvicto.ca>
2024-03-28 09:35:00 -0400

    "changement"

--- src/models/pokemons.model.js
+++ src/models/pokemons.model.js
@@ -32,12 +32,10 @@ Pokemons.trouverUnType = (type) => {
     return new Promise((resolve, reject) => {
         var requete = ""
         if (type == []){
-            requete = `SELECT id, nom, type_primaire, type_secondaire, pv, attaque, defense 
-            FROM pokemon;`;
+            requete = 'SELECT id, nom, type_primaire, type_secondaire, pv, attaque, defense FROM pokemon;';
         }
         else{
-            requete = `SELECT id, nom, type_primaire, type_secondaire, pv, attaque, defense 
-            FROM pokemon WHERE type_primaire = $1`;
+            requete = 'SELECT id, nom, type_primaire, type_secondaire, pv, attaque, defense FROM pokemon WHERE type_primaire = $1';
         }
 
         const params = [type]
@@ -58,8 +56,7 @@ Pokemons.trouverUnType = (type) => {
 Pokemons.ajouterUnPokemon = (nom, type_primaire, type_secondaire, pv, attaque, defense) => {
     return new Promise((resolve, reject) => {
 
-        const requete = `INSERT INTO pokemon (nom, type_primaire, type_secondaire, pv, attaque, defense) 
-        VALUES ($1, $2, $3, $4, $5, $6); `;
+        const requete = 'INSERT INTO pokemon (nom, type_primaire, type_secondaire, pv, attaque, defense) VALUES ($1, $2, $3, $4, $5, $6); ';
         const params = [nom, type_primaire, type_secondaire, pv, attaque, defense]
 
         sql.query(requete, params, (erreur, resultat) => {
@@ -77,8 +74,7 @@ Pokemons.ajouterUnPokemon = (nom, type_primaire, type_secondaire, pv, attaque, d
 Pokemons.modifierUnPokemon = (nom, type_primaire, type_secondaire, pv, attaque, defense, id) => {
     return new Promise((resolve, reject) => {
 
-        const requete = `UPDATE pokemon SET nom = $1, type_primaire = $2, type_secondaire = $3, 
-         pv = $4, attaque = $5, defense = $6 WHERE id = $7;`;
+        const requete = 'UPDATE pokemon SET nom = $1, type_primaire = $2, type_secondaire = $3, pv = $4, attaque = $5, defense = $6 WHERE id = $7;';
         const params = [nom, type_primaire, type_secondaire, pv, attaque, defense, parseInt(id)];
 
         sql.query(requete, params, (erreur, resultat) => {
@@ -95,7 +91,7 @@ Pokemons.modifierUnPokemon = (nom, type_primaire, type_secondaire, pv, attaque, 
 Pokemons.supprimerUnPokemon = (id) => {
     return new Promise((resolve, reject) => {
 
-        const requete = `DELETE FROM pokemon WHERE id = $1;`;
+        const requete = 'DELETE FROM pokemon WHERE id = $1;';
         const params = [id];
         
         sql.query(requete, params, (erreur, resultat) => {
